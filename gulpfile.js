b5872f6cc566bd05c9643386f0c801ec5068cf43
rayzru <rbdc9vtrc8prvmg>
2016-04-06 16:36:19 +0300

    integrating unit test with Karma created build gulp file update version required used from libs to cdnjs.cloudflare.com

---
+++ gulpfile.js
@@ -0,0 +1,28 @@
+var gulp = require('gulp');
+var concat = require('gulp-concat');
+var uglify = require('gulp-uglify');
+var notify = require('gulp-notify');
+var karma = require('karma').Server;
+
+
+gulp.task('js', function () {
+	return gulp.src('src/**/*.js')
+		.pipe(concat('event-parser.min.js'))
+		.pipe(uglify())
+		.pipe(gulp.dest('dist'));
+	//.pipe(notify({ message: 'Finished minifying JavaScript'}));
+});
+
+
+gulp.task('watch', function () {
+	gulp.watch('src/**/*.js', ['js']);
+});
+
+gulp.task('test', function (done) {
+	new karma({
+		configFile: __dirname + '/test/karma.conf.js',
+		singleRun: true
+	}, done).start();
+});
+
+gulp.task('default', ['js', 'watch']);
